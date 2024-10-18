139e45a89f7cbc44ad2f2d6042959870ac05fd18
Eveshogweore Alle <alleeveshogweyore@gmail.com>
2024-10-18 13:00:41 +0100

    Import config file properly.

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .config import Config
 
 # Initialize the database object
 db = SQLAlchemy()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
 
     # Load configuration from config.py
-    app.config.from_object('app.config.Config')
+    app.config.from_object(config_class)
 
     # Initialize the database
     db.init_app(app)
 
     # Register routes
-    from .routes import main
+    from app.routes import main
     app.register_blueprint(main)
 
     return app
